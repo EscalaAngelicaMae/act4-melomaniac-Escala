2d900e2efeee04aa436aab64c68a057dcac43ff3
EscalaAngelicaMae <angelicaescala22@gmail.com>
2022-04-19 21:50:16 +0800

    Commit for SongNumber4

---
+++ SongNumber4.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MeloChills </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to MeloChills</h1>
+                <p class="lead">In the middle of every moments lies a song worth listening. Tune in to MeloChills as we play songs according to your mood. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Dulo</h2>";
+                        echo "<p class='lead '>by The Juans</p>";
+                        
+                          
+                        echo "<p>Nakatingin na naman sa kawalan tinatago ang tunay na nararamdaman</p>";
+                        echo "<p>Hangggang kailan ko 'to kakayanin hanggang saan kita uunawain</p>";
+                        echo "<p>Pa'no ipaglalaban ang pagmamahal kung sa pananatili ay nasasakal</p>";
+                        echo "<p>Nahihirapan na ating mga damdamin 'di malaman kung pa'no aaminin</p>";
+                        
+                        
+                        echo "<p>Naubos na ang luha at mga salita siguro'y 'di sapat aking pagmamahal</p>";
+                        
+                        
+                        echo "<p>Ito na ba ang hinihintay ang pagwawakas ng paglalakbay</p>";
+                        echo "<p>Lahat nama'y ibinigay ngunit balewala lang kaya paalam,</p>";
+                        echo "<p>Kailangan na yatang harapin na ito ay 'di para sa atin</p>";
+                        echo "<p>Mahirap man na tanggapin hanggang dito lang tayo</p>";
+                        echo "<p>ito na ang dulo (ito na ang dulo)</p>";
+                        
+                        
+                        echo "<p>Bibitawan at palalayain ka kasama ang pangako't alaala</p>";
+                        echo "<p>Ngunit hindi kita pipilitin hahayaan ko na lang ito 'yong mapansin</p>";
+                        
+                        
+                         echo "<p>Naubos na ang luha at mga salita siguro'y 'di sapat aking pagmamahal</p>";
+                        
+                        
+                        echo "<p>Ito na ba ang hinihintay ang pagwawakas ng paglalakbay</p>";
+                        echo "<p>Lahat nama'y ibinigay ngunit balewala lang kaya paalam,</p>";
+                        echo "<p>Kailangan na yatang harapin na ito ay 'di para sa atin</p>";
+                        echo "<p>Mahirap man na tanggapin hanggang dito lang tayo</p>";
+                        echo "<p>ito na ang dulo</p>";
+                         
+                         
+                        echo "<p>Gusto mang kumapit mahirap ipilit ang 'di para sa atin</p>";
+                        echo "<p>Hihintayin ko na maramdaman mo ito na ang dulo</p>";
+                        
+                          
+                        echo "<p>Ito na ba ang hinihintay ang pagwawakas ng paglalakbay</p>";
+                        echo "<p>Lahat nama'y ibinigay ngunit balewala lang kaya paalam,</p>";
+                        echo "<p>Kailangan na yatang harapin na ito ay 'di para sa atin</p>";
+                        echo "<p>Mahirap man na tanggapin hanggang dito lang tayo</p>";
+                        echo "<p>ito na ang dulo (ito na ang dulo)</p>";   
+                        echo "<p>Hanggang dito lang tayo ito na ang dulo</p>";
+                        
+                        
+                        
+                         echo "<p>SONG SUGGESTIONS</p>";
+
+                        ?>
+                         <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber1.php">Pagsamo</a></li>
+                        
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber2.php">Paraluman</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="SongNumber3.php">Ang Wakas</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="SongNumber5.php">Tahanan</a></li>
+                    </ul>
+                        
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angelica Mae Escala</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
