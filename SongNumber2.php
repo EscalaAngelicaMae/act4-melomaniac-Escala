70f457b6718e1ec0f543b96538a15fba1f556b43
EscalaAngelicaMae <angelicaescala22@gmail.com>
2022-04-19 21:48:18 +0800

    Commit for SongNumber2

---
+++ SongNumber2.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MeloChills </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to MeloChills</h1>
+                <p class="lead">In the middle of every moments lies a song worth listening. Tune in to MeloChills as we play songs according to your mood. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Paraluman</h2>";
+                        echo "<p class='lead '>by Adie</p>";
+                        echo "<p>Sa unang tingin, agad na nahumaling</p>";
+                        echo "<p>Sa nagniningning mong mga mata</p>";
+                        echo "<p>Ika'y isang bituin na nagmula sa langit</p>";
+                        echo "<p>Hindi ko mawari ang taglay mong tinatangi</p>";
+                        echo "<p>Sadya namang nakakabighani</p>";
+                        echo "<p>'Di maipaliwanag ang nararamdaman (ooh)</p>";
+                        echo "<p>Namumukadkad ang aking ligaya,</p>";
+                        echo "<p>Sa tuwing ika'y papalapit na</p>";
+                        echo "<p>Hawakan mo ang aking kamay</p>";
+                        echo "<p>'Oh, Paraluman</p>";
+                        echo "<p>Ika'y akin nang dadalhin sa</p>";
+                        echo "<p>'Di mo inaasahang paraiso (hoo-hoo)</p>";
+                        echo "<p>Palagi kitang aawitan ng Kundiman</p>";
+                        echo "<p>'Di magsasawa, 'di ka pababayaan</p>";
+                        echo "<p>Isasayaw kita hanggang sa walang hanggan (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Mga gunita na laging naiisip (naiisip)</p>";
+                        echo "<p>Sumisilip (sumisilip) ang itinakda ng mahiwaga</p>";
+                        echo "<p>Liwanag na dulot mo, nagbigay sinag sa madilim kong mundo (ooh-ooh-oh)</p>";
+                        echo "<p>Ibang-iba ako kapag ikaw na ang kapiling</p>";
+                        echo "<p>Sumisiping ang buwan at mga bituin</p>";
+                        echo "<p>Na para bang sumasang-ayon sa atin ang kalawakan (kalawakan)</p>";
+                        echo "<p>Namumukadkad ang aking ligaya</p>";
+                        echo "<p>Sa tuwing ika'y papalapit na</p>";
+                        echo "<p>Hawakan mo ang aking kamay (hoo-hoo)</p>";
+                        echo "<p>Oh, Paraluman</p>";
+                        echo "<p>Ika'y akin nang dadalhin sa</p>";
+                        echo "<p>'Di mo inaasahang paraiso (hoo-hoo)</p>";
+                        echo "<p>Palagi kitang aawitan ng Kundiman</p>";
+                        echo "<p>'Di magsasawa, 'di ka pababayaan</p>";
+                        echo "<p>Isasayaw kita hanggang sa walang hanggan (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Para-pa-pa-para-Paraluman (hoo-hoo, hoo-hoo)</p>";
+                        echo "<p>Himig ng tadhana (ah, ah)</p>";
+                        echo "<p>Sa atin ay tumutugma na (tumutugma na)</p>";
+                        echo "<p>Himig ng tadhana (ah, ah)</p>";
+                        echo "<p>Sa atin ay tumutugma na (tumutugma na)</p>";
+                        echo "<p>Himig ng tadhana (ah, ah)</p>";
+                        echo "<p>Sa atin ay tumutugma na (ooh-ooh-ooh, hoo-hoo, hoo),</p>";
+                        echo "<p>Oh, Paraluman</p>";
+         
+                        echo "<p>Ika'y akin nang dadalhin sa</p>";
+                        echo "<p>'Di mo inaasahang paraiso (hoo-hoo)</p>";
+                        echo "<p>Palagi kitang aawitan ng Kundiman</p>";
+                        echo "<p>'Di magsasawa, 'di ka pababayaan</p>";
+                        echo "<p>Isasayaw kita, mamahalin kita</p>";
+                        echo "<p>Hanggang sa walang hanggan (hoo-hoo, hoo-hoo)</p>";
+                        
+                         echo "<p>SONG SUGGESTIONS</p>";
+
+                        ?>
+                         <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber1.php">Pagsamo</a></li>
+                        
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber3.php">Ang Wakas</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="SongNumber4.php">Dulo</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="SongNumber5.php">Tahanan</a></li>
+                    </ul>
+                        
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angelica Mae Escala</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
