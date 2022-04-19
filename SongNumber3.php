38e2fc71a156d57605b511162842756ee627745d
EscalaAngelicaMae <angelicaescala22@gmail.com>
2022-04-19 21:49:37 +0800

    Commit for SongNumber3

---
+++ SongNumber3.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> MeloChills </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to MeloChills</h1>
+                <p class="lead">In the middle of every moments lies a song worth listening. Tune in to MeloChills as we play songs according to your mood. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Ang wakas</h2>";
+                        echo "<p class='lead '>by Arthur Miguel ft. Trisha Macapagal</p>";
+                        
+                          
+                        echo "<p>Mahirap ba'ng ipilit ang lumaban 'pag hindi na kaya?</p>";
+                        echo "<p>Saan na kukuha ng lakas?</p>";
+                        echo "<p>Ibuhos man lahat-lahat, wala pa rin itong pag-asa</p>";
+                        echo "<p>Kung mag-isa kang lalaban</p>";
+                        
+                        
+                        echo "<p>Sa pag takbo ng oras, unti-unting kumupas</p>";
+                        echo "<p>Ang dating wagas ay magwawakas</p>";
+                        
+                        
+                        echo "<p>Masisisi mo ba kung ayaw na talaga?</p>";
+                        echo "<p>Kung ang pag-ibig mo, tuluyang maglaho</p>";
+                        echo "<p>Oh, ba't nagbago bigla? Mga titig ay nag-iba</p>";
+                        echo "<p>Ika'y lumalayo, tadhana ba ito?</p>";
+                        
+                        
+                        echo "<p>Kapag damdamin na'ng nagsalita</p>";
+                        echo "<p>Wala ka nang magagawa kun'di sundin ito kahit ayaw</p>";
+                        echo "<p>Wala na ngang natirira, lahat-lahat ay naglaho na</p>";
+                        echo "<p>Konting pilit pa'y masusugatan</p>";
+                        echo "<p>Bumitaw ka na</p>";
+                        
+                         
+                        echo "<p>Sa pag takbo ng oras, unti-unting kumupas</p>";
+                        echo "<p>Ang dating wagas ay magwawakas</p>";
+                        
+                          
+                        echo "<p>Masisisi mo ba kung ayaw na talaga?</p>";
+                        echo "<p>Kung ang pag-ibig mo, tuluyang maglaho</p>";
+                        echo "<p>Oh, ba't nagbago bigla? Mga titig ay nag-iba</p>";
+                        echo "<p>Ika'y lumalayo, tadhana ba ito?</p>";
+                        
+                         
+                        echo "<p>Tayo'y nagkamali, tayo ay nasugatan</p>";
+                        echo "<p>Maling galaw, lahat ay sasabit</p>";
+                        echo "<p>Ito na ba'ng huli?</p>";
+                        echo   "<p>Tayo'y magpapaalam na sa ating nakaraan</p>";
+                        echo "<p>At bibitawan</p>";
+                        
+                     echo "<p>Masisisi mo ba kung ayaw na talaga?</p>";
+                        echo "<p>Kung ang pag-ibig mo, tuluyang maglaho</p>";
+                        echo "<p>Oh, ba't nagbago bigla? Mga titig ay nag-iba</p>";
+                        echo "<p>Ika'y lumalayo, tadhana ba ito?</p>";
+                        
+                        
+                         echo "<p>SONG SUGGESTIONS</p>";
+
+                        ?>
+                         <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber1.php">Pagsamo</a></li>
+                        
+                        <li class="nav-item"><a class="nav-link text-light" href="SongNumber2.php">Paraluman</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="SongNumber4.php">Dulo</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="SongNumber5.php">Tahanan</a></li>
+                    </ul>
+                        
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Angelica Mae Escala</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
